e9a680897ad69c9a5a2e31168cdc424bd03a5e0e
izZziCOS <50332439+izZziCOS@users.noreply.github.com>
2020-01-20 16:48:00 +0200

    new modules

---
+++ task/src/routes/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Input} from 'semantic-ui-react'
+
+const InputExample = () => <Input placeholder='Search...' />
+
+class MainPage extends React.Component{
+    render(){
+        return(
+            <p>labas</p>
+        )
+    }
+    
+}
+export default MainPage
